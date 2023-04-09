d6b6d75316bd188ba89402648acd40119c3bd1ea
rohan future devoloper <xrohaislam373@gmail.com>
2023-04-09 23:59:10 +0600

    function and Prameter

---
+++ function.js
@@ -0,0 +1,12 @@
+// funtion declaration
+
+function startFan(){
+    console.log("stand up");
+    console.log("switch er kase jaw");
+    console.log("switch ta dew")
+}
+// call the funcion
+startFan()
+console.log("walking the morning")
+// startFan()
+// startFan()
---
+++ parameter.js
@@ -0,0 +1,5 @@
+function bringShingara(money){
+    console.log("mama shingara den");
+    console.log(money);
+}
+bringShingara(300);
