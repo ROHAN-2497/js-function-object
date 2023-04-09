8777f5f23cd6a8f7ac40e84b2d84286fdb86f622
rohan future devoloper <xrohaislam373@gmail.com>
2023-04-10 03:09:52 +0600

    properties and object files

---
+++ object.js
@@ -0,0 +1,19 @@
+var student = {
+    name : "rohan",
+    class: "seven",
+    roll : 02, 
+    marks : 90,
+
+}
+console.log(student.marks);
+var mobile = {
+    brand : "samsung",
+    model : "A54",
+    color : "blue",
+    price : 40000,
+    storage : "64GB"
+}
+console.log(mobile.color);
+mobile.brand = 'apple';
+mobile.color = "black"; 
+console.log(mobile);
---
+++ properties.js
@@ -0,0 +1,17 @@
+var shoppingCart = {
+    book: 4,
+    mouse : 2,
+    keyboard : 6,
+    bag : 5,
+
+}
+  propertyName = "book";
+  console.log(propertyName)
+
+
+
+
+// // bookCount = shoppingCart.book;
+// bookCount = shoppingCart["book"];
+// console.log(bookCount);
+// console.log(shoppingCart)
