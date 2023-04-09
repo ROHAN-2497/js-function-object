373dbb1c13c27ff9af55a79b18f6c4fa55f6bd92
rohan future devoloper <xrohaislam373@gmail.com>
2023-04-10 01:03:49 +0600

    return js file

--- parameter.js
+++ parameter.js
@@ -1,5 +1,15 @@
-function bringShingara(money){
-    console.log("mama shingara den");
-    console.log(money);
+// function bringShingara(money){
+//     console.log("mama shingara den");
+//     console.log(money);
+// }
+// bringShingara(300);
+
+// function add(number1,number2){
+//     console.log("ey gula number:",  number1, number2)
+// }
+// add(10,203);
+
+function sum(a, b, c, d, e, f, f, g, h, i){
+    console.log('sob gils', a, b, c, d, e, f, g, h, i)
 }
-bringShingara(300);
+sum(1, 2, 3, 4, 5, 6,);
---
+++ return.js
@@ -0,0 +1,17 @@
+// function add(number1, number2,){
+//     console.log(number1, number2)
+//     var sum = number1 + number2
+//     return sum;
+// }
+// var total = add(20, 30)
+// console.log('total', total)
+ 
+
+function shingara(money){
+    var sum = money + money
+    console.log(money)
+    return sum
+}
+// console.log(300)
+var total = shingara(500,)
+shingara(total)
