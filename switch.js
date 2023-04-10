bd8f169e37c585f0ea170b185c3cd0ceae2c9d0e
rohan future devoloper <xrohaislam373@gmail.com>
2023-04-10 16:03:35 +0600

    switch.js files

---
+++ switch.js
@@ -0,0 +1,43 @@
+const color = "green"
+if (color === "green"){
+    console.log("tumi amr green friends")
+}
+else if ( color === "blue")
+{console.log('tuumi amr blue friends')}
+else if(color === "red"){
+    console.log('tumi amr red friends')
+}
+else if (color === 'white'){
+    console.log('tumi amr white firend');
+}
+
+else if (color === 'yellow'){
+    console.log( 'tumi amr himu firend');
+}
+else{
+    console.log('tumi amr black black friend');
+}
+
+/*
+switch
+*/
+ const color1 = "blue";
+switch(color1){
+    case 'green' :
+        console.log('you are my green friend')
+        break ;
+        case 'red':
+            console.log('you are my red friend');
+            break;
+            case 'blue' :
+                console.log('you are my blue friend');
+                break ;
+                case 'white' :
+                    console.log('you are my white firend')
+                    break ;
+                    case 'yellow' :
+                        console.log("you are my yellow firend")
+                        break ;
+                        default :
+                        console.log("you are my black black friend");
+}
